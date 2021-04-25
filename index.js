9a36f3b0a6e0b31f0777f483dce5457a6da9b7ac
Vmorais22 <victormoraisllahi@gmail.com>
2021-04-25 11:18:34 +0200

    trying

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ require('dotenv').config({path: '../src/var.env'});
 
 const mongoose = require('mongoose');
 const app = require('./app')
-const port = 3901;
+const port = 3900;
 
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://yayos:yayos@yayos.64oge.mongodb.net/yayos?retryWrites=true&w=majority', {useNewUrlParser: true})
-        .then(() => {
-            console.log('La conexión a la base de datos correcta');
+try {
+    mongoose.connect('mongodb+srv://yayos:yayos@yayos.64oge.mongodb.net/yayos?retryWrites=true&w=majority',
+        {useNewUrlParser: true}, () => console.log(" Mongoose is connected")
+    )
+} catch (e) {
+    console.log("could not connect");
+}
 
-            app.listen(port, () => {
-                console.log("servidor creado en puerto " + port)
-            })
-        });
-
+const dbConnection = mongoose.connection;
+dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
